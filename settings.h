05afb072bf09efae065f65764cf4fc14c2530bcc
me <znamsiparolata4esno@abv.bg>
2023-12-05 15:26:14 +0200

    update

--- settings.h
+++ settings.h
@@ -21,7 +21,7 @@
 #define RESPAWN_TIME_MS 20e3
 #define MINION_SPAWN_INTERVAL_MS 2100
 
-#define HEALTH_STATES 8 // clarity vs bandwidth
+#define HEALTH_STATES 8 // clarity vs bandwidth (and style)
 
 // levels
 
