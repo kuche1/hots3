7b9f5581a37caa0689c323b55221c8d41f270c44
kuche1 <znamsiparolata4esno@abv.bg>
2024-07-29 23:15:55 +0300

    update

--- entities.c
+++ entities.c
@@ -1,9 +1,9 @@
 
 #include "entities.h"
 
-struct player *get_entity_at(int pos_y, int pos_x, struct player entities[ENTITIES_MAX]){
+struct player * get_entity_at(int pos_y, int pos_x, struct player entities[ENTITIES_MAX]){
     for(int entity_idx=0; entity_idx<ENTITIES_MAX; ++entity_idx){
-        struct player *entity = &entities[entity_idx];
+        struct player * entity = &entities[entity_idx];
 
         if(!entity->alive){
             continue;
--- entities.h
+++ entities.h
@@ -4,6 +4,6 @@
 
 #include "player.h"
 
-struct player *get_entity_at(int pos_y, int pos_x, struct player entities[ENTITIES_MAX]);
+struct player * get_entity_at(int pos_y, int pos_x, struct player entities[ENTITIES_MAX]);
 
 #endif
